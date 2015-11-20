0054bd30ca0dad83eba14de3246b454f1de4d0d3
Antonio Cervone <ant.cervone@gmail.com>
2015-11-20 15:34:02 +0100

    add bc_list class

--- bc.hpp
+++ bc.hpp
@@ -12,7 +12,7 @@ public:
   void init(uint const numPts)
   {
     vec = bool_array::Constant(numPts, false);
-    for(auto&id: *this)
+    for(auto& id: *this)
     {
       vec[id] = true;
     }
@@ -25,4 +25,35 @@ public:
 
   bool_array vec;
   scalarFun_T value;
+};
+
+class bc_list: public std::vector<bc_ess>
+{
+public:
+  bc_list(std::initializer_list<bc_ess> list):
+    std::vector<bc_ess>(list)
+    {}
+
+  void init(uint const numPts)
+  {
+    for(auto& bc: *this)
+    {
+      bc.init(numPts);
+    }
+    vec = bool_array::Constant(numPts, false);
+    for(uint i=0; i<numPts; ++i)
+    {
+      for(auto& bc: *this)
+      {
+        vec[i] = vec[i] || bc.vec[i];
+      }
+    }
+  }
+
+  bool is_constrained(Point const& p) const
+  {
+    return vec[p.id];
+  }
+
+  bool_array vec;
 };

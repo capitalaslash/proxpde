2bbb53f187ce0686d628ab2bafa0306fb980e172
Antonio Cervone <ant.cervone@gmail.com>
2015-11-26 15:19:57 +0100

    rename shape functions

--- reffe.hpp
+++ reffe.hpp
@@ -5,24 +5,25 @@
 struct RefLineP1
 {
   static uint const numPts = 2U;
+  typedef Line GeoElem_T;
   typedef Eigen::Vector2d localVec_T;
   typedef Eigen::Matrix2d localMat_T;
   typedef std::array<Point*,numPts> pointList_T;
 
-  static std::array<scalarFun_T,numPts> const phi;
-  static std::array<vectorFun_T,numPts> const dphi;
+  static std::array<scalarFun_T,numPts> const phiFun;
+  static std::array<vectorFun_T,numPts> const dphiFun;
   static localMat_T const massMat;
   static localMat_T const gradMat;
   static double constexpr volume = 2.L;
 };
 
-std::array<scalarFun_T,RefLineP1::numPts> const RefLineP1::phi =
+std::array<scalarFun_T,RefLineP1::numPts> const RefLineP1::phiFun =
 {
   [] (Vec3 const & p) { return 0.5*(1-p(0)); },
   [] (Vec3 const & p) { return 0.5*(1+p(0)); }
 };
 
-std::array<vectorFun_T,RefLineP1::numPts> const RefLineP1::dphi =
+std::array<vectorFun_T,RefLineP1::numPts> const RefLineP1::dphiFun =
 {
   [] (Vec3 const & p) { return Vec3( 0.5L, 0.0, 0.0); },
   [] (Vec3 const & p) { return Vec3(-0.5L, 0.0, 0.0); }

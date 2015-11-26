9fbc4fe4f8ad7ae9506eb86ccbf6dd53c29d80e2
Antonio Cervone <ant.cervone@gmail.com>
2015-11-26 15:21:14 +0100

    use CurFE and new qr rule

--- fe.hpp
+++ fe.hpp
@@ -3,16 +3,20 @@
 #include "def.hpp"
 #include "geo.hpp"
 #include "qr.hpp"
-
+#include "curfe.hpp"
 void buildProblem(std::shared_ptr<Mesh<Line>> const meshPtr,
                   scalarFun_T const& rhs,
                   bc_list const& bcs,
                   std::vector<Tri>& coefficients,
                   Vec& b)
 {
+  typedef CurFE<RefLineP1,GaussQR<3>> FE_T;
+  FE_T curFE;
   for(auto &e: meshPtr->elementList)
   {
-    double J = RefLineP1::volume / e.volume();
+    curFE.reinit(e);
+
+    // double const J = curFE.J;
 
     Line::localVec_T elemRhs = Line::localVec_T::Zero(Line::numPts, 1);
 
@@ -21,7 +25,7 @@ void buildProblem(std::shared_ptr<Mesh<Line>> const meshPtr,
     // C clear constrained rows/cols
     // h is the vector of local constraint values
 
-    Line::localMat_T elemMat_c = J*RefLineP1::gradMat;
+    Line::localMat_T elemMat_c = curFE.stiffMat;
     Line::localVec_T elemRhs_c = elemRhs;
 
     for(auto& bc: bcs)
@@ -38,14 +42,14 @@ void buildProblem(std::shared_ptr<Mesh<Line>> const meshPtr,
         }
       }
       elemMat_c = C * elemMat_c * C;
-      elemRhs_c = C * (elemRhs_c - J*RefLineP1::gradMat * h);
+      elemRhs_c = C * (elemRhs_c - curFE.stiffMat * h);
 
       for(uint i=0; i<Line::numPts; ++i)
       {
         if(bc.is_constrained(*e.pointList[i]))
         {
-          elemMat_c(i,i) = J;
-          elemRhs_c(i) = J*h[i];
+          elemMat_c(i,i) = 1./curFE.J;
+          elemRhs_c(i) = h[i]/curFE.J;
         }
       }
     }
@@ -56,9 +60,18 @@ void buildProblem(std::shared_ptr<Mesh<Line>> const meshPtr,
       b(id_i) += elemRhs_c(i);
       if(!bcs.is_constrained(*e.pointList[i]))
       {
-        // b(id_i) += rectangleInt(e, i, rhs)/J;
-        b(id_i) += gauss3Int(e, i, rhs)/J;
+        // b(id_i) += J * rectangleInt(e, i, rhs);
+        // b(id_i) += J * gauss3Int(e, curFE.phi[i], rhs);
+        for(uint q=0; q<FE_T::QR_T::numPts; ++q)
+        {
+          double const f = rhs(curFE.qpoint[q]);
+          b(id_i) += curFE.JxW[q]*curFE.phi(i, q) * f;
+        }
       }
+    }
+    for(uint i=0; i<Line::numPts; ++i)
+    {
+      const id_T id_i = e.pointList[i]->id;
       for(uint j=0; j<Line::numPts; ++j)
       {
         const id_T id_j = e.pointList[j]->id;
@@ -66,9 +79,9 @@ void buildProblem(std::shared_ptr<Mesh<Line>> const meshPtr,
       }
 
       // // neumann bc
-      // if(right.vec[id_i])
+      // if(id_i == meshPtr->pointList.size()-1)
       // {
-      //   b(id_i) += right.value(*e.pointList[i]);
+      //   b(id_i) += 1.;
       // }
     }
   }

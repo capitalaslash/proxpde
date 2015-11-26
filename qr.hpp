beefd9f6c3a876db9bf3c82966f0176824c78675
Antonio Cervone <ant.cervone@gmail.com>
2015-11-26 15:20:23 +0100

    unify gauss qr rules in class

--- qr.hpp
+++ qr.hpp
@@ -4,47 +4,35 @@
 #include "geo.hpp"
 #include "reffe.hpp"
 
-double rectangleInt(Line const& e, id_T i, scalarFun_T rhs)
+template <uint N>
+struct GaussQR
 {
-  // volume * shape fun midpoint * rhs fun midpoint
-  return 2.*RefLineP1::phi[i](Vec3::Zero(3))*rhs(e.midpoint());
-}
+  static uint const numPts = N;
 
-static Eigen::Vector2d const gauss2w = Eigen::Vector2d::Constant(1.L);
+  static Eigen::Array<double,N,1> const w;
+  static std::array<Vec3,N> const n;
+};
+
+template<> Eigen::Array<double,1,1> const GaussQR<1>::w =
+  Eigen::Array<double,1,1>::Constant(2.L);
+template<> std::array<Vec3,1> const GaussQR<1>::n =
+{
+  Vec3( 0., 0., 0.),
+};
+
 static double constexpr sqrt13rd = std::sqrt(1.L/3);
-static std::array<Vec3,2> const gauss2n
+template<> Eigen::Array<double,2,1> const GaussQR<2>::w = {1.L, 1.L};
+template<> std::array<Vec3,2> const GaussQR<2>::n =
 {
   Vec3(-sqrt13rd, 0., 0.),
   Vec3( sqrt13rd, 0., 0.)
 };
 
-double gauss2Int(Line const& e, id_T i, scalarFun_T rhs)
-{
-  double sum = 0.;
-  for(uint g=0; g<2; ++g)
-  {
-    Vec3 const node = e.midpoint() + 0.5*gauss2n[g]*e.volume();
-    sum += gauss2w(g) * RefLineP1::phi[i](gauss2n[g]) * rhs(node);
-  }
-  return sum;
-}
-
-static Eigen::Vector3d const gauss3w = {5.L/9, 8.L/9, 5.L/9};
 static double constexpr sqrt35th = std::sqrt(3.L/5);
-static std::array<Vec3,3> const gauss3n
+template<> Eigen::Array<double,3,1> const GaussQR<3>::w = {5.L/9, 8.L/9, 5.L/9};
+template<> std::array<Vec3,3> const GaussQR<3>::n =
 {
   Vec3(-sqrt35th, 0., 0.),
   Vec3(       0., 0., 0.),
   Vec3( sqrt35th, 0., 0.)
 };
-
-double gauss3Int(Line const& e, id_T i, scalarFun_T rhs)
-{
-  double sum = 0.;
-  for(uint g=0; g<3; ++g)
-  {
-    Vec3 const node = e.midpoint() + 0.5*gauss3n[g]*e.volume();
-    sum += gauss3w(g) * RefLineP1::phi[i](gauss3n[g]) * rhs(node);
-  }
-  return sum;
-}

a857540ebfaa914d883da7aeb8b29a9b6b8c119e
Antonio Cervone <ant.cervone@gmail.com>
2015-11-30 15:10:06 +0100

    add RefLineP2 (not working)

--- reffe.cpp
+++ reffe.cpp
@@ -24,3 +24,33 @@ RefLineP1::LocalMat_T const RefLineP1::massMat =
 RefLineP1::LocalMat_T const RefLineP1::gradMat =
   (Eigen::Matrix2d() <<  0.5L, -0.5L,
                         -0.5L,  0.5L ).finished();
+
+std::array<Vec3,RefLineP2::numPts> const RefLineP2::points =
+{
+  Vec3(-1.L, 0., 0.),
+  Vec3(  0., 0., 0.),
+  Vec3( 1.L, 0., 0.)
+};
+
+std::array<scalarFun_T,RefLineP2::numPts> const RefLineP2::phiFun =
+{
+  [] (Vec3 const & p) { return 0.5*(p(0)-1)*p(0); },
+  [] (Vec3 const & p) { return 0.5*(p(0)+1)*p(0); },
+  [] (Vec3 const & p) { return 1-p(0)*p(0); }
+};
+
+std::array<vectorFun_T,RefLineP2::numPts> const RefLineP2::dphiFun =
+{
+  [] (Vec3 const & p) { return Vec3(0.5*(2.*p(0)-1.), 0.0, 0.0); },
+  [] (Vec3 const & p) { return Vec3(0.5*(2.*p(0)+1.), 0.0, 0.0); },
+  [] (Vec3 const & p) { return Vec3(      1.-2.*p(0), 0.0, 0.0); }
+};
+
+RefLineP2::LocalMat_T const RefLineP2::massMat =
+  (Eigen::Matrix3d() << 0., 0., 0.,
+                        0., 0., 0.,
+                        0., 0., 0. ).finished();
+RefLineP2::LocalMat_T const RefLineP2::gradMat =
+  (Eigen::Matrix3d() << 0., 0., 0.,
+                        0., 0., 0.,
+                        0., 0., 0. ).finished();
--- reffe.hpp
+++ reffe.hpp
@@ -18,3 +18,19 @@ struct RefLineP1
   static LocalMat_T const gradMat;
   static double constexpr volume = 2.L;
 };
+
+struct RefLineP2
+{
+  static uint const numPts = 3U;
+  typedef Line GeoElem_T;
+  typedef Eigen::Vector3d LocalVec_T;
+  typedef Eigen::Matrix3d LocalMat_T;
+  typedef std::array<Point*,numPts> PointList_T;
+
+  static std::array<Vec3,numPts> const points;
+  static std::array<scalarFun_T,numPts> const phiFun;
+  static std::array<vectorFun_T,numPts> const dphiFun;
+  static LocalMat_T const massMat;
+  static LocalMat_T const gradMat;
+  static double constexpr volume = 2.L;
+};

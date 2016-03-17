5c569e8d3f7896f0a9d40f869ab20d82229b207e
Antonio Cervone <ant.cervone@gmail.com>
2016-03-17 17:42:12 +0100

    add dof object to fespace

--- fespace.hpp
+++ fespace.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "def.hpp"
+#include "dof.hpp"
 #include "curfe.hpp"
 
 template <typename Mesh,
@@ -11,12 +12,15 @@ struct FESpace
   typedef Mesh Mesh_T;
   typedef RefFE RefFE_T;
   typedef QR QR_T;
+  typedef DOF<Mesh, RefFE> DOF_T;
   typedef CurFE<RefFE,QR> CurFE_T;
 
   explicit FESpace(std::shared_ptr<Mesh> const mesh):
-    meshPtr(mesh)
+    meshPtr(mesh),
+    dof(*mesh)
   {}
 
   std::shared_ptr<Mesh> const meshPtr;
   CurFE_T curFE;
+  DOF_T dof;
 };

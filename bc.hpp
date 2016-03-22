323f2cbd98e731016654c14c1987edb755b02c2f
Antonio Cervone <ant.cervone@gmail.com>
2016-03-22 16:14:08 +0100

    set bcs on dofs and not points

--- bc.hpp
+++ bc.hpp
@@ -6,19 +6,19 @@
 
 typedef Eigen::Array<bool,Eigen::Dynamic,1> bool_array;
 
-template <typename Mesh>
+template <typename FESpace>
 class bc_ess
 {
 public:
-  bc_ess(Mesh const& mesh, marker_T m, scalarFun_T v):
+  bc_ess(FESpace const& feSpace, marker_T m, scalarFun_T v):
     marker(m),
     value(v)
   {
-    for(auto& p: mesh.pointList)
+    for(auto& p: feSpace.meshPtr->pointList)
     {
       if(p.marker == marker)
       {
-        point_set.insert(p.id);
+        point_set.insert(p.dof_ids[0]);
         // std::cout << "inserting " << p.id << " " << p.marker << " " << marker << std::endl;
       }
     }
@@ -33,23 +33,23 @@ public:
     }
   }
 
-  bool is_constrained(Point const& p) const
+  bool is_constrained(DOFid_T const& id) const
   {
-    return vec[p.id];
+    return vec[id];
   }
 
   marker_T marker;
   scalarFun_T value;
   bool_array vec;
-  std::set<id_T> point_set;
+  std::set<DOFid_T> point_set;
 };
 
-template <typename Mesh>
-class bc_list: public std::vector<bc_ess<Mesh>>
+template <typename FESpace>
+class bc_list: public std::vector<bc_ess<FESpace>>
 {
 public:
-  bc_list(std::initializer_list<bc_ess<Mesh>> list):
-    std::vector<bc_ess<Mesh>>(list)
+  bc_list(std::initializer_list<bc_ess<FESpace>> list):
+    std::vector<bc_ess<FESpace>>(list)
     {}
 
   void init(uint const numPts)

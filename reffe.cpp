1739aae603310d47d2999b794b22515ee5684def
Antonio Cervone <ant.cervone@gmail.com>
2016-03-22 16:03:51 +0100

    distinguish between fe element and geo element

--- reffe.cpp
+++ reffe.cpp
@@ -1,5 +1,7 @@
 #include "reffe.hpp"
 
+std::array<uint,4> constexpr RefLineP1::dof_place;
+
 std::array<Vec3,RefLineP1::numPts> const RefLineP1::points =
 {
   Vec3(-1.L, 0., 0.),
@@ -25,14 +27,20 @@ RefLineP1::LocalMat_T const RefLineP1::gradMat =
   (Eigen::Matrix2d() <<  0.5L, -0.5L,
                         -0.5L,  0.5L ).finished();
 
-std::array<Vec3,RefLineP2::numPts> const RefLineP2::points =
+std::array<uint,4> constexpr RefLineP2::dof_place;
+
+static Point line_p0{-1., 0., 0.};
+static Point line_p1{ 1., 0., 0.};
+Line const RefLineP2::geoElem = Line{{&line_p0, &line_p1}};
+
+std::array<Vec3,RefLineP2::numFuns> const RefLineP2::points =
 {
   Vec3(-1.L, 0., 0.),
   Vec3(  0., 0., 0.),
   Vec3( 1.L, 0., 0.)
 };
 
-std::array<scalarFun_T,RefLineP2::numPts> const RefLineP2::phiFun =
+std::array<scalarFun_T,RefLineP2::numFuns> const RefLineP2::phiFun =
 {
   [] (Vec3 const & p) { return 0.5*(p(0)-1)*p(0); },
   [] (Vec3 const & p) { return 0.5*(p(0)+1)*p(0); },
@@ -55,6 +63,8 @@ RefLineP2::LocalMat_T const RefLineP2::gradMat =
                         0., 0., 0.,
                         0., 0., 0. ).finished();
 
+std::array<uint,4> constexpr RefTriangleP1::dof_place;
+
 std::array<scalarFun_T,RefTriangleP1::numPts> const RefTriangleP1::phiFun =
 {
   [] (Vec3 const & p) { return 1.L - p(0) - p(1); },
@@ -68,6 +78,8 @@ std::array<vectorFun_T,RefTriangleP1::numPts> const RefTriangleP1::dphiFun =
   [] (Vec3 const & p) { return Vec3( 1.L,  0.0, 0.0); },
   [] (Vec3 const & p) { return Vec3( 0.0,  1.L, 0.0); }
 };
+
+std::array<uint,4> constexpr RefQuadQ1::dof_place;
 
 std::array<scalarFun_T,RefQuadQ1::numPts> const RefQuadQ1::phiFun =
 {
--- reffe.hpp
+++ reffe.hpp
@@ -9,7 +9,7 @@ uint constexpr numDOFs()
   return 1 * RefElem::dof_place[0] +
     // RefElem::numFaces * RefElem::dof_place[1] +
     // RefElem::numEdges * RefElem::dof_place[2] +
-    RefElem::numPts * RefElem::dof_place[3];
+    RefElem::GeoElem_T::numPts * RefElem::dof_place[3];
 }
 
 struct RefLineP1
@@ -32,20 +32,31 @@ struct RefLineP1
 
 struct RefLineP2
 {
-  static uint constexpr dim = 1U;
-  static uint constexpr numPts = 3U;
-  static std::array<uint,4> constexpr dof_place{1,0,0,1};
   typedef Line GeoElem_T;
+  static GeoElem_T const geoElem;
+  static uint constexpr dim = 1U;
+  static uint constexpr numFuns = 3U;
+  static std::array<uint,4> constexpr dof_place{1,0,0,1};
   typedef Eigen::Vector3d LocalVec_T;
   typedef Eigen::Matrix3d LocalMat_T;
-  typedef std::array<Point*,numPts> PointList_T;
 
-  static std::array<Vec3,numPts> const points;
-  static std::array<scalarFun_T,numPts> const phiFun;
-  static std::array<vectorFun_T,numPts> const dphiFun;
+  static std::array<Vec3,numFuns> const points;
+  static std::array<scalarFun_T,numFuns> const phiFun;
+  static std::array<vectorFun_T,numFuns> const dphiFun;
   static LocalMat_T const massMat;
   static LocalMat_T const gradMat;
   static double constexpr volume = 2.L;
+
+  static std::array<Vec3,numFuns> dofPts(GeoElem const & e)
+  {
+     std::array<Vec3,numFuns> dofPts =
+     {
+       e.pointList[0]->coord,
+       e.pointList[1]->coord,
+       e.midpoint()
+     };
+     return std::move(dofPts);
+  }
 };
 
 struct RefTriangleP1

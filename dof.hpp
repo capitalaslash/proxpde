681ce72542ae405af660abef3b398cd83566b6e3
Antonio Cervone <ant.cervone@gmail.com>
2016-03-25 14:56:10 +0100

    add dofs on faces

--- dof.hpp
+++ dof.hpp
@@ -12,11 +12,11 @@ struct DOF
   explicit DOF(Mesh & mesh):
     _rows(mesh.elementList.size())
   {
-    std::cout << "_clms = " << _clms << std::endl;
     elemMap.resize(_rows);
     ptMap.resize(mesh.pointList.size());
     std::vector<DOFid_T> elemDOFs(mesh.elementList.size(), DOFidNotSet);
     std::map<std::set<id_T>,DOFid_T> edgeDOFs;
+    std::map<std::set<id_T>,DOFid_T> faceDOFs;
 
     uint dof_count = 0;
     for(auto const & e: mesh.elementList)
@@ -74,6 +74,33 @@ struct DOF
         }
       }
 
+      // check if there dofs on the faces
+      if(RefFE::dof_place[1])
+      {
+        for(uint i=0; i<Mesh::Elem_T::numFaces; i++)
+        {
+          std::set<id_T> faceIDs;
+          for(uint j=0; j<Mesh::Elem_T::Face_T::numPts; j++)
+          {
+            faceIDs.insert(e.pointList[Mesh::Elem_T::elemToFace[i][j]]->id);
+          }
+
+          // check if dofs have already been assigned to this face
+          auto it = faceDOFs.find(faceIDs);
+          if(it == faceDOFs.end())
+          {
+            faceDOFs[faceIDs] = dof_count;
+            elemMap[e.id][local_dof_count] = dof_count;
+            dof_count++;
+            local_dof_count++;
+          }
+          else
+          {
+            elemMap[e.id][local_dof_count] = faceDOFs[faceIDs];
+            local_dof_count++;
+          }
+        }
+      }
       // check if there dofs on the element
       if(RefFE::dof_place[0])
       {

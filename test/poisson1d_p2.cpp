f125a80356cc4020695ea6931f5dc9da99ffa78e
Antonio Cervone <ant.cervone@gmail.com>
2016-04-14 15:28:55 +0200

    add Timer class

--- test/poisson1d_p2.cpp
+++ test/poisson1d_p2.cpp
@@ -5,6 +5,7 @@
 #include "bc.hpp"
 #include "assembly.hpp"
 #include "iomanager.hpp"
+#include "timer.hpp"
 
 #include <iostream>
 
@@ -26,6 +27,7 @@ scalarFun_T exact_sol = [] (Vec3 const& p)
 
 int main(int argc, char* argv[])
 {
+  MilliTimer t;
   uint const numPts = (argc < 2)? 21 : std::stoi(argv[1]);
 
   Vec3 const origin{0., 0., 0.};
@@ -33,14 +35,20 @@ int main(int argc, char* argv[])
 
   std::shared_ptr<Mesh_T> meshPtr(new Mesh_T);
 
+  t.start();
   MeshBuilder<Elem_T> meshBuilder;
   meshBuilder.build(meshPtr, origin, length, {numPts, 0, 0});
+  std::cout << "mesh build: " << t << " ms" << std::endl;
 
+  t.start();
   FESpace_T feSpace(meshPtr);
+  std::cout << "fespace: " << t << " ms" << std::endl;
 
+  t.start();
   bc_ess<FESpace_T> left(feSpace, side::LEFT, [] (Vec3 const&) {return 0.;});
   bc_list<FESpace_T> bcs{feSpace, {left}};
   bcs.init();
+  std::cout << "bcs: " << t << " ms" << std::endl;
 
   Mat A(feSpace.dof.totalNum, feSpace.dof.totalNum);
   Vec b = Vec::Zero(feSpace.dof.totalNum);
@@ -48,24 +56,30 @@ int main(int argc, char* argv[])
   AssemblyStiffness<FESpace_T> stiffness(feSpace);
   AssemblyAnalyticalRhs<FESpace_T> f(rhs, feSpace);
 
+  t.start();
   Builder builder(A, b);
   builder.buildProblem(stiffness, bcs);
   builder.buildProblem(f, bcs);
   builder.closeMatrix();
+  std::cout << "fe build: " << t << " ms" << std::endl;
 
+  t.start();
   Var sol{"u"};
   Eigen::SparseLU<Mat, Eigen::COLAMDOrdering<int>> solver;
   solver.analyzePattern(A);
   solver.factorize(A);
   sol.data = solver.solve(b);
+  std::cout << "solve: " << t << " ms" << std::endl;
 
   Var exact{"exact", feSpace.dof.totalNum};
   interpolateAnalyticalFunction(exact_sol, feSpace, exact.data);
   Var error{"e"};
   error.data = sol.data - exact.data;
 
+  t.start();
   IOManager<FESpace_T> io{"sol_poisson1d_p2.xmf", feSpace};
   io.print({sol, exact, error});
+  std::cout << "output: " << t << " ms" << std::endl;
 
   double norm = error.data.norm();
   std::cout << "the norm of the error is " << norm << std::endl;
--- test/poisson2dquad.cpp
+++ test/poisson2dquad.cpp
@@ -5,6 +5,7 @@
 #include "bc.hpp"
 #include "assembly.hpp"
 #include "iomanager.hpp"
+#include "timer.hpp"
 
 #include <iostream>
 
@@ -26,6 +27,7 @@ scalarFun_T exact_sol = [] (Vec3 const& p)
 
 int main(int argc, char* argv[])
 {
+  MilliTimer t;
   uint const numPts_x = (argc < 3)? 11 : std::stoi(argv[1]);
   uint const numPts_y = (argc < 3)? 11 : std::stoi(argv[2]);
 
@@ -34,39 +36,51 @@ int main(int argc, char* argv[])
 
   std::shared_ptr<Mesh_T> meshPtr(new Mesh_T);
 
+  t.start();
   MeshBuilder<Elem_T> meshBuilder;
   meshBuilder.build(meshPtr, origin, length, {numPts_x, numPts_y, 0});
+  std::cout << "mesh build: " << t << " ms" << std::endl;
 
+  t.start();
   FESpace_T feSpace(meshPtr);
+  std::cout << "fespace: " << t << " ms" << std::endl;
 
+  t.start();
   bc_ess<FESpace_T> left(feSpace, side::LEFT, [] (Vec3 const&) {return 0.;});
   bc_ess<FESpace_T> bottom(feSpace, side::BOTTOM, [] (Vec3 const&) {return 0.;});
   bc_list<FESpace_T> bcs{feSpace, {left, bottom}};
   bcs.init();
+  std::cout << "bcs: " << t << " ms" << std::endl;
 
   Mat A(feSpace.dof.totalNum, feSpace.dof.totalNum);
   Vec b = Vec::Zero(feSpace.dof.totalNum);
 
   AssemblyStiffness<FESpace_T> stiffness(feSpace);
 
+  t.start();
   Builder builder(A, b);
   builder.buildProblem(stiffness, bcs);
   builder.buildProblem(AssemblyAnalyticalRhs<FESpace_T>(rhs, feSpace), bcs);
   builder.closeMatrix();
+  std::cout << "fe build: " << t << " ms" << std::endl;
 
+  t.start();
   Var sol{"u"};
   Eigen::SparseLU<Mat, Eigen::COLAMDOrdering<int>> solver;
   solver.analyzePattern(A);
   solver.factorize(A);
   sol.data = solver.solve(b);
+  std::cout << "solve: " << t << " ms" << std::endl;
 
   Var exact{"exact", feSpace.dof.totalNum};
   interpolateAnalyticalFunction(exact_sol, feSpace, exact.data);
   Var error{"e"};
   error.data = sol.data - exact.data;
 
+  t.start();
   IOManager<FESpace_T> io{"sol_poisson2dquad.xmf", feSpace};
   io.print({sol, exact, error});
+  std::cout << "output: " << t << " ms" << std::endl;
 
   double norm = error.data.norm();
   std::cout << "the norm of the error is " << norm << std::endl;

eee7316d2ac5d4f7e1ddd5346cf6955d3a6c06c5
Antonio Cervone <ant.cervone@gmail.com>
2016-05-18 17:42:17 +0200

    add blockmatrix class

---
+++ blockmatrix.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+namespace detail
+{
+static constexpr auto sum()
+{
+  return 0;
+}
+
+template<typename T1, typename... T>
+static constexpr auto sum(T1 s, T... ts)
+{
+  return s + sum(ts...);;
+}
+
+template<typename T>
+constexpr auto accumulator(T const & array, size_t const end =  std::tuple_size<T>::value)
+{
+  // static_assert(N <= std::tuple_size<T>::value, "cannot accumulate beyond array size");
+  typename T::value_type sum(typename T::value_type(0));
+  for(uint i=0; i<end; ++i)
+  {
+    sum += array[i];
+  }
+  return sum;
+}
+
+template <typename T>
+constexpr auto array_accu(T const sizes)
+{
+  T offsets(sizes);
+  for(uint i=0; i<sizes.size(); i++)
+  {
+    offsets[i] = accumulator(sizes, i);
+  }
+  return offsets;
+}
+}
+
+template <uint... Is>
+struct Block
+{
+  static uint const totalSize = detail::sum(Is...);
+  typedef array<uint,sizeof...(Is)> Array_T;
+  static Array_T constexpr size = {{Is...}};
+  static Array_T constexpr offset = detail::array_accu<Array_T>({{Is...}});
+};
+template <uint... Is>
+typename Block<Is...>::Array_T constexpr Block<Is...>::size;
+template <uint... Is>
+typename Block<Is...>::Array_T constexpr Block<Is...>::offset;
+
+template <uint... Blocks>
+struct BlockMatrix
+{
+  typedef Block<Blocks...> B_T;
+  static uint const size = B_T::totalSize;
+  typedef Eigen::Matrix<double, size, size, 0, size, size> Data_T;
+
+  BlockMatrix():
+    data(Data_T::Zero())
+  {}
+
+  template<uint I, uint J>
+  Eigen::Block<Data_T,
+               B_T::size[I],
+               B_T::size[J]>
+  block()
+  {
+    return Eigen::Block<Data_T,
+                        B_T::size[I],
+                        B_T::size[J]>
+             (data.derived(), B_T::offset[I], B_T::offset[J]);
+  }
+
+  Data_T data;
+};
+
+template <uint... Blocks>
+std::ostream & operator<<(std::ostream & out, BlockMatrix<Blocks...> const & m)
+{
+  for(uint i=0; i<sizeof...(Blocks); i++)
+  {
+    out << BlockMatrix<Blocks...>::B_T::size[i] << " "
+        << BlockMatrix<Blocks...>::B_T::offset[i] << std::endl;
+  }
+  out << std::endl << m.data;
+  return out;
+}
---
+++ test/blockmatrix.cpp
@@ -0,0 +1,18 @@
+#include "def.hpp"
+#include "blockmatrix.hpp"
+
+int main()
+{
+  Eigen::Matrix<double,2,2> b;
+  b << 1,1,1,1;
+
+  BlockMatrix<2,2,3> m;
+  auto ref = m.block<1,1>();
+  ref = b;
+  auto block = m.block<1,2>();
+  block << 2,2,2,2,2,2;
+
+  std::cout << m << std::endl;
+
+  return 0;
+}

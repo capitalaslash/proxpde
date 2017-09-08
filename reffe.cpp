76a37267f5d506df4e76f7546ad609467f5f774d
Antonio Cervone <ant.cervone@gmail.com>
2017-09-08 17:27:09 +0200

    update reference fes and add quad8

--- reffe.cpp
+++ reffe.cpp
@@ -1,33 +1,47 @@
 #include "reffe.hpp"
 
 // ----------------------------------------------------------------------------
+array<uint,4> constexpr RefPointP1::dof_place;
+array<array<uint,0>,0> constexpr RefPointP1::dofOnFacet;
+
+array<RefPointP1::Vec_T,RefPointP1::numFuns> const RefPointP1::points =
+{{
+  Vec_T::Constant( 1.L)
+}};
+
+array<ScalarFun<1>,RefPointP1::numFuns> const RefPointP1::phiFun =
+{{
+  [] (Vec_T const & p) { return 1.L; },
+}};
+
+// ----------------------------------------------------------------------------
 array<uint,4> constexpr RefLineP1::dof_place;
 array<array<uint,1>,2> constexpr RefLineP1::dofOnFacet;
 
 array<RefLineP1::Vec_T,RefLineP1::numFuns> const RefLineP1::points =
-{
+{{
   Vec_T::Constant(-1.L),
   Vec_T::Constant( 1.L)
-};
+}};
 
 array<ScalarFun<1>,RefLineP1::numFuns> const RefLineP1::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 0.5*(1-p(0)); },
   [] (Vec_T const & p) { return 0.5*(1+p(0)); }
-};
+}};
 
 array<onedFun_T,RefLineP1::numFuns> const RefLineP1::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T::Constant(-0.5L); },
   [] (Vec_T const & p) { return Vec_T::Constant(+0.5L); }
-};
+}};
 
-RefLineP1::LocalMat_T const RefLineP1::massMat =
-  (RefLineP1::LocalMat_T() << 2.L/3, 1.L/3,
-                        1.L/3, 2.L/3 ).finished();
-RefLineP1::LocalMat_T const RefLineP1::gradMat =
-  (RefLineP1::LocalMat_T() <<  0.5L, -0.5L,
-                        -0.5L,  0.5L ).finished();
+//RefLineP1::LocalMat_T const RefLineP1::massMat =
+//  (RefLineP1::LocalMat_T() << 2.L/3, 1.L/3,
+//                        1.L/3, 2.L/3 ).finished();
+//RefLineP1::LocalMat_T const RefLineP1::gradMat =
+//  (RefLineP1::LocalMat_T() <<  0.5L, -0.5L,
+//                        -0.5L,  0.5L ).finished();
 
 // ----------------------------------------------------------------------------
 array<uint,4> constexpr RefLineP2::dof_place;
@@ -38,69 +52,69 @@ static Point line_p1{ 1., 0., 0.};
 Line const RefLineP2::geoElem = Line{{&line_p0, &line_p1}};
 
 array<RefLineP2::Vec_T,RefLineP2::numFuns> const RefLineP2::points =
-{
+{{
   Vec_T::Constant(-1.L),
   Vec_T::Constant( 0.L),
   Vec_T::Constant( 1.L)
-};
+}};
 
 array<ScalarFun<1>,RefLineP2::numFuns> const RefLineP2::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 0.5*p(0)*(p(0)-1.); },
   [] (Vec_T const & p) { return 0.5*p(0)*(p(0)+1.); },
   [] (Vec_T const & p) { return 1.-p(0)*p(0); }
-};
+}};
 
 array<onedFun_T,RefLineP2::numFuns> const RefLineP2::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T::Constant(p(0)-0.5); },
   [] (Vec_T const & p) { return Vec_T::Constant(p(0)+0.5); },
   [] (Vec_T const & p) { return Vec_T::Constant(-2.*p(0)); }
-};
+}};
 
-RefLineP2::LocalMat_T const RefLineP2::massMat =
-  (RefLineP2::LocalMat_T() << 0., 0., 0.,
-                        0., 0., 0.,
-                        0., 0., 0. ).finished();
-RefLineP2::LocalMat_T const RefLineP2::gradMat =
-  (RefLineP2::LocalMat_T() << 0., 0., 0.,
-                        0., 0., 0.,
-                        0., 0., 0. ).finished();
+//RefLineP2::LocalMat_T const RefLineP2::massMat =
+//  (RefLineP2::LocalMat_T() << 0., 0., 0.,
+//                        0., 0., 0.,
+//                        0., 0., 0. ).finished();
+//RefLineP2::LocalMat_T const RefLineP2::gradMat =
+//  (RefLineP2::LocalMat_T() << 0., 0., 0.,
+//                        0., 0., 0.,
+//                        0., 0., 0. ).finished();
 
 // ----------------------------------------------------------------------------
 array<uint,4> constexpr RefTriangleP1::dof_place;
 array<array<uint,2>,3> constexpr RefTriangleP1::dofOnFacet;
 
 array<scalarTwodFun_T,RefTriangleP1::numFuns> const RefTriangleP1::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 1.L - p(0) - p(1); },
   [] (Vec_T const & p) { return p(0); },
   [] (Vec_T const & p) { return p(1); }
-};
+}};
 
 array<twodFun_T,RefTriangleP1::numFuns> const RefTriangleP1::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T(-1.L, -1.L); },
   [] (Vec_T const & p) { return Vec_T( 1.L,  0.L); },
   [] (Vec_T const & p) { return Vec_T( 0.L,  1.L); }
-};
+}};
 
 // ----------------------------------------------------------------------------
 array<uint,4> constexpr RefTriangleP2::dof_place;
 array<array<uint,3>,3> constexpr RefTriangleP2::dofOnFacet;
 
 array<scalarTwodFun_T,RefTriangleP2::numFuns> const RefTriangleP2::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 2.*(1.-p(0)-p(1))*(0.5-p(0)-p(1)); },
   [] (Vec_T const & p) { return 2.*p(0)*(p(0)-0.5); },
   [] (Vec_T const & p) { return 2.*p(1)*(p(1)-0.5); },
   [] (Vec_T const & p) { return 4.*(1.-p(0)-p(1))*p(0); },
   [] (Vec_T const & p) { return 4.*p(0)*p(1); },
   [] (Vec_T const & p) { return 4.*p(1)*(1.-p(0)-p(1)); },
-};
+}};
 
 array<twodFun_T,RefTriangleP2::numFuns> const RefTriangleP2::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T(4.*p(0)+4.*p(1)-3.,
                                     4.*p(0)+4.*p(1)-3.); },
   [] (Vec_T const & p) { return Vec_T(4.*p(0)-1.,
@@ -113,34 +127,70 @@ array<twodFun_T,RefTriangleP2::numFuns> const RefTriangleP2::dphiFun =
                                     4.*p(0)); },
   [] (Vec_T const & p) { return Vec_T(-4.*p(1),
                                     -4.*p(1)+4.*(1.-p(0)-p(1))); }
-};
+}};
 
 // ----------------------------------------------------------------------------
 array<uint,4> constexpr RefQuadQ1::dof_place;
 array<array<uint,2>,4> constexpr RefQuadQ1::dofOnFacet;
 
 array<scalarTwodFun_T,RefQuadQ1::numFuns> const RefQuadQ1::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 0.25*(1.-p(0))*(1.-p(1)); },
   [] (Vec_T const & p) { return 0.25*(1.+p(0))*(1.-p(1)); },
   [] (Vec_T const & p) { return 0.25*(1.+p(0))*(1.+p(1)); },
   [] (Vec_T const & p) { return 0.25*(1.-p(0))*(1.+p(1)); }
-};
+}};
 
 array<twodFun_T,RefQuadQ1::numFuns> const RefQuadQ1::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T(-0.25*(1.-p(1)), -0.25*(1.-p(0))); },
   [] (Vec_T const & p) { return Vec_T( 0.25*(1.-p(1)), -0.25*(1.+p(0))); },
   [] (Vec_T const & p) { return Vec_T( 0.25*(1.+p(1)),  0.25*(1.+p(0))); },
   [] (Vec_T const & p) { return Vec_T(-0.25*(1.+p(1)),  0.25*(1.-p(0))); }
-};
+}};
+
+// ----------------------------------------------------------------------------
+array<uint,4> constexpr RefQuadP2::dof_place;
+array<array<uint,3>,4> constexpr RefQuadP2::dofOnFacet;
+
+array<scalarTwodFun_T,RefQuadP2::numFuns> const RefQuadP2::phiFun =
+{{
+  [] (Vec_T const & p) { return -0.25*(1.-p(0))*(1.-p(1))*(1+p(0)+p(1)); },
+  [] (Vec_T const & p) { return -0.25*(1.+p(0))*(1.-p(1))*(1-p(0)+p(1)); },
+  [] (Vec_T const & p) { return -0.25*(1.+p(0))*(1.+p(1))*(1-p(0)-p(1)); },
+  [] (Vec_T const & p) { return -0.25*(1.-p(0))*(1.+p(1))*(1+p(0)-p(1)); },
+  [] (Vec_T const & p) { return  0.5*(1.-p(0))*(1.+p(0))*(1.-p(1)); },
+  [] (Vec_T const & p) { return  0.5*(1.+p(0))*(1.-p(1))*(1.+p(1)); },
+  [] (Vec_T const & p) { return  0.5*(1.-p(0))*(1.+p(0))*(1.+p(1)); },
+  [] (Vec_T const & p) { return  0.5*(1.-p(0))*(1.-p(1))*(1.+p(1)); }
+}};
+
+array<twodFun_T,RefQuadP2::numFuns> const RefQuadP2::dphiFun =
+{{
+  [] (Vec_T const & p) { return Vec_T(0.25*(1-p(1))*(2.*p(0)   +p(1)),
+                                      0.25*(1-p(0))*(   p(0)+2.*p(1))); },
+  [] (Vec_T const & p) { return Vec_T(0.25*(1-p(1))*(2.*p(0)   -p(1)),
+                                      0.25*(1+p(0))*(  -p(0)+2.*p(1))); },
+  [] (Vec_T const & p) { return Vec_T(0.25*(1+p(1))*(2.*p(0)   +p(1)),
+                                      0.25*(1+p(0))*(   p(0)+2.*p(1))); },
+  [] (Vec_T const & p) { return Vec_T(0.25*(1+p(1))*(2.*p(0)   -p(1)),
+                                      0.25*(1-p(0))*(  -p(0)+2.*p(1))); },
+  [] (Vec_T const & p) { return Vec_T(-p(0)*(1.-p(1)),
+                                     -0.5*(1-p(0)*p(0))); },
+  [] (Vec_T const & p) { return Vec_T(+0.5*(1-p(1)*p(1)),
+                                     -(1.+p(0))*p(1)); },
+  [] (Vec_T const & p) { return Vec_T(-p(0)*(1.+p(1)),
+                                     +0.5*(1-p(0)*p(0))); },
+  [] (Vec_T const & p) { return Vec_T(-0.5*(1-p(1)*p(1)),
+                                      -(1.-p(0))*p(1)); }
+}};
 
 // ----------------------------------------------------------------------------
 array<uint,4> constexpr RefQuadQ2::dof_place;
 array<array<uint,3>,4> constexpr RefQuadQ2::dofOnFacet;
 
 array<scalarTwodFun_T,RefQuadQ2::numFuns> const RefQuadQ2::phiFun =
-{
+{{
   [] (Vec_T const & p) { return 0.25*p(0)*(p(0)-1.)*p(1)*(p(1)-1.); },
   [] (Vec_T const & p) { return 0.25*p(0)*(p(0)+1.)*p(1)*(p(1)-1.); },
   [] (Vec_T const & p) { return 0.25*p(0)*(p(0)+1.)*p(1)*(p(1)+1.); },
@@ -150,10 +200,10 @@ array<scalarTwodFun_T,RefQuadQ2::numFuns> const RefQuadQ2::phiFun =
   [] (Vec_T const & p) { return  0.5*(1.-p(0)*p(0))*p(1)*(p(1)+1.); },
   [] (Vec_T const & p) { return  0.5*p(0)*(p(0)-1.)*(1.-p(1)*p(1)); },
   [] (Vec_T const & p) { return      (1.-p(0)*p(0))*(1.-p(1)*p(1)); }
-};
+}};
 
 array<twodFun_T,RefQuadQ2::numFuns> const RefQuadQ2::dphiFun =
-{
+{{
   [] (Vec_T const & p) { return Vec_T(0.5*(p(0)-0.5)*p(1)*(p(1)-1.),
                                      0.5*p(0)*(p(0)-1.)*(p(1)-0.5)); },
   [] (Vec_T const & p) { return Vec_T(0.5*(p(0)+0.5)*p(1)*(p(1)-1.),
@@ -172,4 +222,4 @@ array<twodFun_T,RefQuadQ2::numFuns> const RefQuadQ2::dphiFun =
                                      -p(0)*(p(0)-1.)*p(1)); },
   [] (Vec_T const & p) { return Vec_T(-2.*p(0)*(1.-p(1)*p(1)),
                                     -2.*(1.-p(0)*p(0))*p(1)); }
-};
+}};
--- reffe.hpp
+++ reffe.hpp
@@ -14,17 +14,29 @@ uint constexpr numDOFs()
 
 struct RefPointP1
 {
-  static uint constexpr dim = 0U;
+  using GeoElem_T = PointElem;
+  using RefFacet_T = NullElem;
+  static GeoElem_T const geoElem;
+  static int constexpr dim = 1;
   static uint constexpr numFuns = 1U;
+  static array<uint,4> constexpr dof_place{{0,0,0,1}};
+  static uint constexpr dofPerFacet = 0U;
+  static array<array<uint,0>,0> constexpr dofOnFacet = {};
   using Vec_T = FVec<dim>;
+  using LocalVec_T = FVec<1>;
+
+  static array<Vec_T,numFuns> const points;
+  static array<ScalarFun<dim>,numFuns> const phiFun;
+  static array<Fun<dim,dim>,numFuns> const dphiFun;
+  static double constexpr volume = 1.L;
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
      array<Vec3,numFuns> dofPts =
-     {
+     {{
        e.pointList[0]->coord
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 
@@ -33,13 +45,13 @@ struct RefLineP1
   using GeoElem_T = Line;
   using RefFacet_T = RefPointP1;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 1U;
+  static int constexpr dim = 1;
   static uint constexpr numFuns = 2U;
-  static array<uint,4> constexpr dof_place{0,0,0,1};
+  static array<uint,4> constexpr dof_place{{0,0,0,1}};
   static uint constexpr dofPerFacet = 1U;
-  static array<array<uint,1>,2> constexpr dofOnFacet = {
-    {{0}, {1}}
-  };
+  static array<array<uint,1>,2> constexpr dofOnFacet = {{
+    {{0}}, {{1}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<2>;
   using LocalMat_T = FMat<2,2>;
@@ -47,18 +59,17 @@ struct RefLineP1
   static array<Vec_T,numFuns> const points;
   static array<ScalarFun<dim>,numFuns> const phiFun;
   static array<Fun<dim,dim>,numFuns> const dphiFun;
-  static LocalMat_T const massMat;
-  static LocalMat_T const gradMat;
+  // static LocalMat_T const massMat;
+  // static LocalMat_T const gradMat;
   static double constexpr volume = 2.L;
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+     array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 
@@ -67,13 +78,13 @@ struct RefLineP2
   using GeoElem_T = Line;
   using RefFacet_T = RefPointP1;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 1U;
+  static int constexpr dim = 1;
   static uint constexpr numFuns = 3U;
-  static array<uint,4> constexpr dof_place{0,0,1,1};
+  static array<uint,4> constexpr dof_place{{0,0,1,1}};
   static uint constexpr dofPerFacet = 1U;
-  static array<array<uint,1>,2> constexpr dofOnFacet = {
-    {{0}, {1}}
-  };
+  static array<array<uint,1>,2> constexpr dofOnFacet = {{
+    {{0}}, {{1}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<numFuns>;
   using LocalMat_T = FMat<numFuns,numFuns>;
@@ -87,13 +98,12 @@ struct RefLineP2
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+     array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
        e.midpoint()
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 
@@ -102,13 +112,13 @@ struct RefTriangleP1
   using GeoElem_T = Triangle;
   using RefFacet_T = RefLineP1;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 2U;
+  static int constexpr dim = 2;
   static uint constexpr numFuns = 3U;
-  static array<uint,4> constexpr dof_place{0,0,0,1};
+  static array<uint,4> constexpr dof_place{{0,0,0,1}};
   static uint constexpr dofPerFacet = 2U;
-  static array<array<uint,2>,3> constexpr dofOnFacet = {
-    {{0,1}, {1,2}, {2,0}}
-  };
+  static array<array<uint,2>,3> constexpr dofOnFacet = {{
+    {{0,1}}, {{1,2}}, {{2,0}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<numFuns>;
   using LocalMat_T = FMat<numFuns,numFuns>;
@@ -119,13 +129,12 @@ struct RefTriangleP1
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+     array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
        e.pointList[2]->coord
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 
@@ -134,13 +143,13 @@ struct RefTriangleP2
   using GeoElem_T = Triangle;
   using RefFacet_T = RefLineP2;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 2U;
+  static int constexpr dim = 2;
   static uint constexpr numFuns = 6U;
-  static array<uint,4> constexpr dof_place{0,0,1,1};
+  static array<uint,4> constexpr dof_place{{0,0,1,1}};
   static uint constexpr dofPerFacet = 3U;
-  static array<array<uint,3>,3> constexpr dofOnFacet = {
-    {{0,1,3}, {1,2,4}, {2,0,5}}
-  };
+  static array<array<uint,3>,3> constexpr dofOnFacet = {{
+    {{0,1,3}}, {{1,2,4}}, {{2,0,5}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<numFuns>;
   using LocalMat_T = FMat<numFuns,numFuns>;
@@ -151,16 +160,15 @@ struct RefTriangleP2
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+     array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
        e.pointList[2]->coord,
        0.5*(e.pointList[0]->coord+e.pointList[1]->coord),
        0.5*(e.pointList[1]->coord+e.pointList[2]->coord),
        0.5*(e.pointList[2]->coord+e.pointList[0]->coord)
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 
@@ -169,13 +177,13 @@ struct RefQuadQ1
   using GeoElem_T = Quad;
   using RefFacet_T = RefLineP1;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 2U;
+  static int constexpr dim = 2;
   static uint constexpr numFuns = 4U;
-  static array<uint,4> constexpr dof_place{0,0,0,1};
+  static array<uint,4> constexpr dof_place{{0,0,0,1}};
   static uint constexpr dofPerFacet = 2U;
-  static array<array<uint,2>,4> constexpr dofOnFacet = {
-    {{0,1}, {1,2}, {2,3}, {3,0}}
-  };
+  static array<array<uint,2>,4> constexpr dofOnFacet = {{
+    {{0,1}}, {{1,2}}, {{2,3}}, {{3,0}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<numFuns>;
   using LocalMat_T = FMat<numFuns,numFuns>;
@@ -186,14 +194,49 @@ struct RefQuadQ1
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+     array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
        e.pointList[2]->coord,
        e.pointList[3]->coord
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
+  }
+};
+
+struct RefQuadP2
+{
+  using GeoElem_T = Quad;
+  using RefFacet_T = RefLineP2;
+  static GeoElem_T const geoElem;
+  static int constexpr dim = 2;
+  static uint constexpr numFuns = 8U;
+  static array<uint,4> constexpr dof_place{{0,0,1,1}};
+  static uint constexpr dofPerFacet = 3U;
+  static array<array<uint,3>,4> constexpr dofOnFacet = {{
+    {{0,1,4}}, {{1,2,5}}, {{2,3,6}}, {{3,0,7}}
+  }};
+  using Vec_T = FVec<dim>;
+  using LocalVec_T = FVec<numFuns>;
+  using LocalMat_T = FMat<numFuns,numFuns>;
+
+  static array<scalarTwodFun_T,numFuns> const phiFun;
+  static array<twodFun_T,numFuns> const dphiFun;
+  static double constexpr volume = 4.L;
+
+  static array<Vec3,numFuns> dofPts(GeoElem const & e)
+  {
+     array<Vec3,numFuns> dofPts {{
+       e.pointList[0]->coord,
+       e.pointList[1]->coord,
+       e.pointList[2]->coord,
+       e.pointList[3]->coord,
+       0.5*(e.pointList[0]->coord+e.pointList[1]->coord),
+       0.5*(e.pointList[1]->coord+e.pointList[2]->coord),
+       0.5*(e.pointList[2]->coord+e.pointList[3]->coord),
+       0.5*(e.pointList[3]->coord+e.pointList[0]->coord)
+     }};
+     return dofPts;
   }
 };
 
@@ -202,13 +245,13 @@ struct RefQuadQ2
   using GeoElem_T = Quad;
   using RefFacet_T = RefLineP2;
   static GeoElem_T const geoElem;
-  static uint constexpr dim = 2U;
+  static int constexpr dim = 2;
   static uint constexpr numFuns = 9U;
-  static array<uint,4> constexpr dof_place{0,1,1,1};
+  static array<uint,4> constexpr dof_place{{0,1,1,1}};
   static uint constexpr dofPerFacet = 3U;
-  static array<array<uint,3>,4> constexpr dofOnFacet = {
-    {{0,1,4}, {1,2,5}, {2,3,6}, {3,0,7}}
-  };
+  static array<array<uint,3>,4> constexpr dofOnFacet = {{
+    {{0,1,4}}, {{1,2,5}}, {{2,3,6}}, {{3,0,7}}
+  }};
   using Vec_T = FVec<dim>;
   using LocalVec_T = FVec<numFuns>;
   using LocalMat_T = FMat<numFuns,numFuns>;
@@ -219,8 +262,7 @@ struct RefQuadQ2
 
   static array<Vec3,numFuns> dofPts(GeoElem const & e)
   {
-     array<Vec3,numFuns> dofPts =
-     {
+    array<Vec3,numFuns> dofPts {{
        e.pointList[0]->coord,
        e.pointList[1]->coord,
        e.pointList[2]->coord,
@@ -230,8 +272,8 @@ struct RefQuadQ2
        0.5*(e.pointList[2]->coord+e.pointList[3]->coord),
        0.5*(e.pointList[3]->coord+e.pointList[0]->coord),
        e.midpoint()
-     };
-     return std::move(dofPts);
+     }};
+     return dofPts;
   }
 };
 

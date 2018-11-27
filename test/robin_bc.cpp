ab8d8f85ba7466105e3a3aca0345bd6ca016e6a3
Antonio Cervone <ant.cervone@gmail.com>
2018-11-27 12:35:22 +0100

    extend robin bc test

--- test/robin_bc.cpp
+++ test/robin_bc.cpp
@@ -8,7 +8,9 @@
 #include "mesh.hpp"
 #include "timer.hpp"
 
-#include <iostream>
+#include <yaml-cpp/yaml.h>
+
+#include <iomanip>
 
 using Elem_T = Line;
 using Mesh_T = Mesh<Elem_T>;
@@ -16,19 +18,32 @@ using FESpace_T = FESpace<Mesh_T,
                           FEType<Elem_T,1>::RefFE_T,
                           FEType<Elem_T,1>::RecommendedQR>;
 
-scalarFun_T rhs = [] (Vec3 const& p)
-{
-  return M_PI*std::sin(M_PI*p(0));
-};
-scalarFun_T exact_sol = [] (Vec3 const& p)
-{
-  return std::sin(M_PI*p(0))/M_PI + p(0);
-};
-
-int main(int argc, char* argv[])
+double test(YAML::Node const & config)
 {
   MilliTimer t;
-  uint const numPts = (argc < 2)? 21 : std::stoi(argv[1]);
+
+  const double hConv = config["hConv"].as<double>();
+  const double temp0 = config["temp0"].as<double>();
+  const double tempA = config["tempA"].as<double>();
+
+  std::cout << "test setup:\n"
+            << "  - hConv = " << hConv << "\n"
+            << "  - temp0 = " << temp0 << "\n"
+            << "  - tempA = " << tempA << std::endl;
+
+  const scalarFun_T rhs = [] (Vec3 const& p)
+  {
+    return M_PI*std::sin(M_PI*p(0));
+  };
+
+  const scalarFun_T exactSol = [hConv, temp0, tempA] (Vec3 const& p)
+  {
+    return std::sin(M_PI*p(0))/M_PI
+            + (1. - hConv * (temp0 - tempA)) * p(0)/(hConv + 1.)
+                    + temp0;
+  };
+
+  uint const numPts = config["n"].as<uint>()+1;
 
   Vec3 const origin{0., 0., 0.};
   Vec3 const length{1., 0., 0.};
@@ -40,37 +55,26 @@ int main(int argc, char* argv[])
   meshBuilder.build(*mesh, origin, length, {{numPts, 0, 0}});
   std::cout << "mesh build: " << t << " ms" << std::endl;
 
-  // rotation matrix
-  double theta = 0. * M_PI / 3.;
-  FMat<3,3> R;
-  R << std::cos(theta), std::sin(theta), 0.0,
-      -std::sin(theta), std::cos(theta), 0.0,
-      0.0, 0.0, 1.0;
-  auto Rt = R.transpose();
-
-  // rotate mesh
-  for (auto & p: mesh->pointList)
-  {
-    p.coord = R * p.coord;
-  }
-
   t.start();
-  FESpace_T feSpace(*mesh);
+  FESpace_T feSpace{*mesh};
   std::cout << "fespace: " << t << " ms" << std::endl;
 
   t.start();
-  BCList<FESpace_T> bcs{feSpace};
-  bcs.addEssentialBC(side::LEFT, [](Vec3 const &){return 0.;});
+  BCList bcs{feSpace};
+  bcs.addEssentialBC(side::LEFT, [temp0](Vec3 const &){return temp0;});
+  // mixed bc: a u + \nabla u = b
+  // - \nabla u = hConv (u - tempA)
+  // -> a = hConv, b = hConv * tempA
+  // hConv -> 0: \nabla u = 0, Neumann homogeneous
+  // hConv -> inf: u = b / a = tempA, Dirichlet
   bcs.addMixedBC(
         side::RIGHT,
-        [](Vec3 const &){return 1.;},
-        [](Vec3 const &){return 1.;});
+        [hConv](Vec3 const &){return hConv;},
+        [hConv, tempA](Vec3 const &){return hConv * tempA;});
   std::cout << "bcs: " << t << " ms" << std::endl;
 
-  AssemblyStiffness<FESpace_T> stiffness(1.0, feSpace);
-
-  auto rotatedRhs = [&Rt] (Vec3 const& p) {return rhs(Rt * p);};
-  AssemblyAnalyticRhs<FESpace_T> f(rotatedRhs, feSpace);
+  AssemblyStiffness stiffness{1.0, feSpace};
+  AssemblyAnalyticRhs f{rhs, feSpace};
 
   t.start();
   Builder builder{feSpace.dof.size};
@@ -90,23 +94,72 @@ int main(int argc, char* argv[])
   sol.data = solver.solve(builder.b);
   std::cout << "solve: " << t << " ms" << std::endl;
 
+  // std::cout << "u:\n" << sol.data << std::endl;
+
   Var exact{"exact"};
-  auto rotatedESol = [&Rt] (Vec3 const& p) {return exact_sol(Rt * p);};
-  interpolateAnalyticFunction(rotatedESol, feSpace, exact.data);
+  interpolateAnalyticFunction(exactSol, feSpace, exact.data);
   Var error{"e"};
   error.data = sol.data - exact.data;
 
   t.start();
-  IOManager<FESpace_T> io{feSpace, "sol_robinbc"};
+  IOManager io{feSpace, fs::path{"output"} / config["filename"].as<std::string>()};
   io.print({sol, exact, error});
   std::cout << "output: " << t << " ms" << std::endl;
 
-  double norm = error.data.norm();
-  std::cout << "the norm of the error is " << norm << std::endl;
-  if(std::fabs(norm - 2.61664e-11) > 1.e-10)
+  return error.data.norm();
+}
+
+int main()
+{
   {
-    std::cerr << "the norm of the error is not the prescribed value" << std::endl;
-    return 1;
+    YAML::Node config;
+    config["n"] = 20;
+    config["hConv"] = 1.0;
+    config["temp0"] = 2.0;
+    config["tempA"] = 1.0;
+    config["filename"] = "sol_robin_test1";
+
+    auto const error = test(config);
+    std::cout << "test1: the norm of the error is "<< std::setprecision(16) << error << std::endl;
+    if(std::fabs(error - 3.071207174712583e-11) > 1.e-15)
+    {
+      std::cerr << "the norm of the error is not the prescribed value" << std::endl;
+      return 1;
+    }
+  }
+
+  {
+    YAML::Node config;
+    config["n"] = 20;
+    config["hConv"] = 0.0;
+    config["temp0"] = 2.0;
+    config["tempA"] = 1.0;
+    config["filename"] = "sol_robin_test2";
+
+    auto const error = test(config);
+    std::cout << "test2: the norm of the error is " << error << std::endl;
+    if(std::fabs(error - 2.607139656824781e-11) > 1.e-15)
+    {
+      std::cerr << "the norm of the error is not the prescribed value" << std::endl;
+      return 2;
+    }
+  }
+
+  {
+    YAML::Node config;
+    config["n"] = 20;
+    config["hConv"] = 1e20;
+    config["temp0"] = 2.0;
+    config["tempA"] = 1.0;
+    config["filename"] = "sol_robin_test3";
+
+    auto const error = test(config);
+    std::cout << "test3: the norm of the error is " << error << std::endl;
+    if(std::fabs(error - 3.753004639986381e-11) > 1.e-15)
+    {
+      std::cerr << "the norm of the error is not the prescribed value" << std::endl;
+      return 3;
+    }
   }
 
   return 0;

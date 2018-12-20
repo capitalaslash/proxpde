657c0efddd1f3a94733b13db1cfbfd3821b008b7
Antonio Cervone <ant.cervone@gmail.com>
2018-12-20 17:37:14 +0100

    improve gmres test

--- test/gmres.cpp
+++ test/gmres.cpp
@@ -8,11 +8,12 @@
 #include "iomanager.hpp"
 #include "timer.hpp"
 
-using Elem_T = Line;
+using Elem_T = Quad;
+uint constexpr order = 1U;
 using Mesh_T = Mesh<Elem_T>;
 using FESpace_T = FESpace<Mesh_T,
-                          FEType<Elem_T,1>::RefFE_T,
-                          FEType<Elem_T,1>::RecommendedQR>;
+                          FEType<Elem_T, order>::RefFE_T,
+                          FEType<Elem_T, order>::RecommendedQR>;
 
 static scalarFun_T rhs = [] (Vec3 const& p)
 {
@@ -26,85 +27,71 @@ static scalarFun_T exactSol = [] (Vec3 const& p)
 int main(int argc, char* argv[])
 {
   MilliTimer t;
-  uint const numPts = (argc < 2)? 21 : std::stoi(argv[1]);
+  uint const numPts = (argc < 2)? 41 : std::stoi(argv[1]);
 
   Vec3 const origin{0., 0., 0.};
-  Vec3 const length{1., 0., 0.};
+  Vec3 const length{1., 1., 0.};
 
   std::unique_ptr<Mesh_T> mesh{new Mesh_T};
 
-  t.start();
+  t.start("mesh build");
   MeshBuilder<Elem_T> meshBuilder;
-  meshBuilder.build(*mesh, origin, length, {{numPts, 0, 0}});
-  filelog << "mesh build: " << t << " ms" << std::endl;
+  meshBuilder.build(*mesh, origin, length, {{numPts, numPts, 0}});
+  t.stop();
 
-  // rotation matrix
-  double theta = M_PI / 3.;
-  FMat<3,3> R;
-  R << std::cos(theta), std::sin(theta), 0.0,
-      -std::sin(theta), std::cos(theta), 0.0,
-      0.0, 0.0, 1.0;
-  auto Rt = R.transpose();
+  t.start("fespace");
+  FESpace_T feSpace{*mesh};
+  t.stop();
 
-  // rotate mesh
-  for (auto & p: mesh->pointList)
-  {
-    p.coord = R * p.coord;
-  }
-
-  t.start();
-  FESpace_T feSpace(*mesh);
-  filelog << "fespace: " << t << " ms" << std::endl;
-
-  t.start();
+  t.start("bcs");
   BCList bcs{feSpace};
   bcs.addEssentialBC(side::LEFT, [](Vec3 const &){return 0.;});
-  filelog << "bcs: " << t << " ms" << std::endl;
+  t.stop();
 
-  AssemblyStiffness stiffness(1.0, feSpace);
-
-  auto rotatedRhs = [&Rt] (Vec3 const& p) {return rhs(Rt * p);};
-  AssemblyAnalyticRhs f(rotatedRhs, feSpace);
-
-  t.start();
+  t.start("fe build");
+  AssemblyStiffness stiffness{1.0, feSpace};
+  AssemblyAnalyticRhs f{rhs, feSpace};
   Builder builder{feSpace.dof.size};
   builder.buildProblem(stiffness, bcs);
   builder.buildProblem(f, bcs);
   builder.closeMatrix();
-  filelog << "fe build: " << t << " ms" << std::endl;
+  t.stop();
 
   // filelog << "A:\n" << builder.A << std::endl;
   // filelog << "b:\n" << builder.b << std::endl;
 
-  t.start();
+  t.start("solver LU");
   Var sol{"u"};
   LUSolver solver;
   solver.analyzePattern(builder.A);
   solver.factorize(builder.A);
   sol.data = solver.solve(builder.b);
-  filelog << "solve LU: " << t << " ms" << std::endl;
+  t.stop();
 
-  t.start();
+  t.start("solver GMRES");
   Var solNew{"uNew"};
   GMRESSolver solverNew;
   solverNew.compute(builder.A);
   solNew.data = solver.solve(builder.b);
-  filelog << "solve GMRES: " << t << " ms" << std::endl;
+  t.stop();
 
   Var exact{"exact"};
-  auto rotatedESol = [&Rt] (Vec3 const& p) {return exactSol(Rt * p);};
-  interpolateAnalyticFunction(rotatedESol, feSpace, exact.data);
+  interpolateAnalyticFunction(exactSol, feSpace, exact.data);
   Var error{"e"};
   error.data = sol.data - exact.data;
 
-  t.start();
+  t.start("output");
   IOManager io{feSpace, "output/sol_gmres"};
   io.print({sol, solNew, exact, error});
-  filelog << "output: " << t << " ms" << std::endl;
+  t.stop();
+
+  filelog << "the test timings are meaningful only in opt mode with a grid" <<
+             " size bigger than 500 x 500" << std::endl;
+  t.print(filelog);
 
   double norm = error.data.norm();
-  std::cout << "the norm of the error is " << norm << std::endl;
-  if(std::fabs(norm - 2.61664e-11) > 1.e-10)
+  std::cout << "the norm of the error is " << std::setprecision(15) << norm << std::endl;
+  if(std::fabs(norm - 5.43963965993362e-12) > 1.e-15)
   {
     std::cerr << "the norm of the error is not the prescribed value" << std::endl;
     return 1;

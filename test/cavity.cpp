4f5e3aeb88e5892b69336e2dccb90d7895e50b46
Antonio Cervone <ant.cervone@gmail.com>
2018-12-20 10:39:59 +0100

    improve cavity test

--- test/cavity.cpp
+++ test/cavity.cpp
@@ -11,18 +11,19 @@
 
 #include <iostream>
 
+static constexpr uint dim = 2;
 using Elem_T = Quad;
 using Mesh_T = Mesh<Elem_T>;
-using QuadraticRefFE = FEType<Elem_T,2>::RefFE_T;
-using LinearRefFE = FEType<Elem_T,1>::RefFE_T;
-using QuadraticQR = FEType<Elem_T,2>::RecommendedQR;
-using FESpaceVel_T = FESpace<Mesh_T,QuadraticRefFE,QuadraticQR,2>;
-using FESpaceP_T = FESpace<Mesh_T,LinearRefFE,QuadraticQR>;
+using QuadraticRefFE = FEType<Elem_T, dim>::RefFE_T;
+using LinearRefFE = FEType<Elem_T, 1>::RefFE_T;
+using QuadraticQR = FEType<Elem_T, dim>::RecommendedQR;
+using FESpaceVel_T = FESpace<Mesh_T, QuadraticRefFE, QuadraticQR, dim>;
+using FESpaceP_T = FESpace<Mesh_T, LinearRefFE, QuadraticQR>;
 
 int main(int argc, char* argv[])
 {
-  uint const numPts_x = (argc < 3)? 4 : std::stoi(argv[1]);
-  uint const numPts_y = (argc < 3)? 4 : std::stoi(argv[2]);
+  uint const numPts_x = (argc < 3)? 5 : std::stoi(argv[1]);
+  uint const numPts_y = (argc < 3)? 5 : std::stoi(argv[2]);
 
   Vec3 const origin{0., 0., 0.};
   Vec3 const length{1., 1., 0.};
@@ -42,24 +43,28 @@ int main(int argc, char* argv[])
   bcsVel.addEssentialBC(side::BOTTOM, zero);
   bcsVel.addEssentialBC(side::TOP, [] (Vec3 const &) {return Vec2(1.0, 0.0);});
   BCList bcsP{feSpaceP};
-  bcsP.addEssentialBC(DofSet_T{1}, [] (Vec3 const &) {return 0.;});
+  // select the point on the bottom boundary in the middle
+  DOFCoordSet pinSet{
+      feSpaceP,
+      [](Vec3 const & p){return std::fabs(p[0] - 0.5) < 1e-12 && std::fabs(p[1]) < 1e-12;}};
+  bcsP.addEssentialBC(pinSet.ids, [] (Vec3 const &) {return 0.;});
 
   auto const dofU = feSpaceVel.dof.size;
   auto const dofP = feSpaceP.dof.size;
-  uint const numDOFs = dofU*FESpaceVel_T::dim + dofP;
+  uint const numDOFs = dofU*dim + dofP;
 
   AssemblyStiffness stiffness(1.0, feSpaceVel);
-  AssemblyGrad grad(-1.0, feSpaceVel, feSpaceP, {0,1}, 0, 2*dofU);
-  AssemblyDiv div(-1.0, feSpaceP, feSpaceVel, {0,1}, 2*dofU, 0);
+  AssemblyGrad grad(-1.0, feSpaceVel, feSpaceP, {0,1}, 0, dofU*dim);
+  AssemblyDiv div(-1.0, feSpaceP, feSpaceVel, {0,1}, dofU*dim, 0);
   // this is required to properly apply the pinning on the pressure
-  AssemblyMass mass(0.0, feSpaceP, {0}, 2*dofU, 2*dofU);
+  AssemblyMass dummy(0.0, feSpaceP, {0}, dofU*dim, dofU*dim);
 
   Builder builder{numDOFs};
   Var sol{"sol"};
   builder.buildProblem(stiffness, bcsVel);
   builder.buildProblem(grad, bcsVel, bcsP);
   builder.buildProblem(div, bcsP, bcsVel);
-  builder.buildProblem(mass, bcsP);
+  builder.buildProblem(dummy, bcsP);
   builder.closeMatrix();
 
   Eigen::UmfPackLU<Mat> solver(builder.A);
@@ -71,24 +76,25 @@ int main(int argc, char* argv[])
 
   Var u{"u", sol.data, 0, dofU};
   Var v{"v", sol.data, dofU, dofU};
-  Var p{"p", sol.data, 2*dofU, dofP};
+  Var p{"p", sol.data, dofU*dim, dofP};
 
   auto uNorm = u.data.norm();
   auto vNorm = v.data.norm();
   auto pNorm = p.data.norm();
 
+  std::cout.precision(12);
   std::cout << "norm of u: " << uNorm << std::endl;
   std::cout << "norm of v: " << vNorm << std::endl;
   std::cout << "norm of p: " << pNorm << std::endl;
 
-  IOManager ioVel{feSpaceVel, "sol_cavity_vel"};
+  IOManager ioVel{feSpaceVel, "output_cavity/vel"};
   ioVel.print({sol});
-  IOManager ioP{feSpaceP, "sol_cavity_p"};
+  IOManager ioP{feSpaceP, "output_cavity/p"};
   ioP.print({p});
 
-  if(std::fabs(uNorm - 2.72045) > 1.e-4 ||
-     std::fabs(vNorm - 0.47505) > 1.e-4 ||
-     std::fabs(pNorm - 20.8162) > 1.e-4)
+  if(std::fabs(uNorm - 3.15947325663) > 1.e-10 ||
+     std::fabs(vNorm - 0.740498457205) > 1.e-10 ||
+     std::fabs(pNorm - 29.3541392171) > 1.e-10)
   {
     std::cerr << "one of the norms is not the prescribed value" << std::endl;
     return 1;

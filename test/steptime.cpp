fe61e786c2164ff9b92cded0073c3e803bfcc0c9
Antonio Cervone <ant.cervone@gmail.com>
2021-09-27 09:14:44 +0200

    add missing steptime test

---
+++ test/steptime.cpp
@@ -0,0 +1,122 @@
+#include "def.hpp"
+
+#include "assembler.hpp"
+#include "assembly.hpp"
+#include "bc.hpp"
+#include "builder.hpp"
+#include "fe.hpp"
+#include "fespace.hpp"
+#include "iomanager.hpp"
+#include "mesh.hpp"
+#include "timer.hpp"
+#include "var.hpp"
+
+int main()
+{
+  YAML::Node config = YAML::LoadFile("steptime.yaml");
+
+  using Elem_T = Quad;
+  using Mesh_T = Mesh<Elem_T>;
+  using QuadraticRefFE = LagrangeFE<Elem_T, 2>::RefFE_T;
+  using LinearRefFE = LagrangeFE<Elem_T, 1>::RefFE_T;
+  using QuadraticQR = LagrangeFE<Elem_T, 2>::RecommendedQR;
+  using FESpaceVel_T = FESpace<Mesh_T, QuadraticRefFE, QuadraticQR, 2>;
+  using FESpaceP_T = FESpace<Mesh_T, LinearRefFE, QuadraticQR>;
+
+  MilliTimer t;
+  auto const numElemsX = config["nx"].as<uint>();
+  auto const numElemsY = config["ny"].as<uint>();
+
+  Vec3 const origin{0., 0., 0.};
+  Vec3 const length{1.0, 0.1, 0.};
+
+  std::unique_ptr<Mesh_T> mesh{new Mesh_T};
+
+  t.start();
+  buildHyperCube(*mesh, origin, length, {numElemsX, numElemsY, 0});
+  std::cout << "mesh build: " << t << " ms" << std::endl;
+
+  t.start();
+  FESpaceVel_T feSpaceVel{*mesh};
+  FESpaceP_T feSpaceP{*mesh, feSpaceVel.dof.size * FESpaceVel_T::dim};
+  std::cout << "fespace: " << t << " ms" << std::endl;
+
+  t.start();
+  auto zero = [](Vec3 const &) { return Vec2::Constant(0.); };
+  auto bcLeft = BCEss{feSpaceVel, side::LEFT};
+  bcLeft << [](Vec3 const & p) { return p[1] > .05 ? Vec2(1.0, 0.0) : Vec2(0.0, 0.0); };
+  auto bcBottom = BCEss{feSpaceVel, side::BOTTOM};
+  bcBottom << zero;
+  auto bcTop = BCEss{feSpaceVel, side::TOP};
+  bcTop << zero;
+  auto const bcsVel = std::tuple{bcLeft, bcBottom, bcTop};
+
+  auto const bcsP = std::tuple{};
+  // DofSet_T pinSet = {1};
+  // auto const bcsP =
+  //     std::tuple{BCEss{feSpaceP, pinSet, [](Vec3 const &) { return 0.; }}};
+  std::cout << "bcs: " << t << " ms" << std::endl;
+
+  // t.start();
+  auto const dofU = feSpaceVel.dof.size;
+  auto const dofP = feSpaceP.dof.size;
+  uint const numDOFs = dofU * FESpaceVel_T::dim + dofP;
+
+  auto const mu = config["mu"].as<double>();
+  AssemblyTensorStiffness stiffness{mu, feSpaceVel};
+  AssemblyGrad grad(-1.0, feSpaceVel, feSpaceP);
+  AssemblyDiv div(-1.0, feSpaceP, feSpaceVel);
+
+  auto const dt = config["timestep"].as<double>();
+  AssemblyScalarMass timeder(1. / dt, feSpaceVel);
+  Vec velOld{2 * dofU};
+  AssemblyProjection timeder_rhs(1. / dt, velOld, feSpaceVel);
+  AssemblyAdvection advection(1.0, velOld, feSpaceVel, feSpaceVel);
+
+  Var sol{"sol", numDOFs};
+  auto ic = [](Vec3 const &) { return Vec2(1., 0.); };
+  interpolateAnalyticFunction(ic, feSpaceVel, sol.data);
+
+  IOManager ioVel{feSpaceVel, "output_steptime/sol_v"};
+  ioVel.print({sol});
+  IOManager ioP{feSpaceP, "output_steptime/sol_p"};
+  Var p{"p", sol.data, 2 * dofU, dofP};
+  ioP.print({p});
+
+  Builder builder{numDOFs};
+  IterSolver solver;
+  auto const ntime = config["numsteps"].as<uint>();
+  double time = 0.0;
+  for (uint itime = 0; itime < ntime; itime++)
+  {
+    time += dt;
+    std::cout << "solving timestep " << itime + 1 << ", time = " << time << std::endl;
+
+    velOld = sol.data;
+
+    builder.buildLhs(std::tuple{timeder, advection, stiffness}, bcsVel);
+    builder.buildRhs(std::tuple{timeder_rhs}, bcsVel);
+    builder.buildCoupling(grad, bcsVel, bcsP);
+    builder.buildCoupling(div, bcsP, bcsVel);
+    builder.closeMatrix();
+
+    solver.compute(builder.A);
+    sol.data = solver.solve(builder.b);
+    auto res = builder.A * sol.data - builder.b;
+    std::cout << "residual norm: " << res.norm() << std::endl;
+
+    builder.clear();
+
+    ioVel.print({sol}, time);
+    p.data = sol.data.block(2 * dofU, 0, dofP, 1);
+    ioP.print({p}, time);
+
+    if ((sol.data - dt * velOld).norm() < 1.e-12)
+    {
+      std::cout << "stationary state has been reached" << std::endl;
+      break;
+    }
+  }
+
+  return 0;
+}

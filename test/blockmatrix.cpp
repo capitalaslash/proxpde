6cee444fd6708f9463416dd20ee206f1370d039c
Antonio Cervone <ant.cervone@gmail.com>
2024-01-11 16:15:18 +0100

    add missing namespace

--- test/blockmatrix.cpp
+++ test/blockmatrix.cpp
@@ -7,7 +7,7 @@ int main()
   Eigen::Matrix<double, 2, 2> b;
   b << 1, 1, 1, 1;
 
-  BlockMatrix<2, 2, 3> m;
+  proxpde::BlockMatrix<2, 2, 3> m;
   auto ref = m.block<1, 1>();
   ref = b;
   auto block = m.block<1, 2>();
